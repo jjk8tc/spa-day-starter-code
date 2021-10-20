21fb5a80b5471a008d00b4fa9ffa23a49bcf9ba2
Kroese <Josh.Kroese@fike.com>
2021-10-19 20:23:23 -0500

    made it through studio but code isn't working

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -4,24 +4,31 @@ import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.Errors;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors, String verify, Model model) {
+        //model.addAttribute("user", user);
         model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+        //model.addAttribute("username", user.getUsername());
+        //model.addAttribute("email", user.getEmail());
         if (user.getPassword().equals(verify)) {
            return "user/index";
+        }
+        else if (errors.hasErrors()){
+            return "user/index";
         }
         else {
             model.addAttribute("error", "Passwords do not match");
